511ca50c6cbfc67dc4021ffb2129e6581916ee52
Palanka777 <dpont@iesebre.com>
2021-11-02 16:59:40 +0100

    Creem el primer test atests/Feature/Video/

---
+++ tests/Feature/Video/VideoTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Feature\Video;
+
+use Carbon\Carbon;
+use Carbon\Exceptions\Exception;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class VideoTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function users_can_view_videos()
+    {
+        // FASE 1 -> Preparacio -> Prepare
+        // WISHFUL PROGRAMMING
+        $video = Video::create([
+           'title' => 'Ubuntu 101',
+           'description' => '# Here description',
+           'url' => 'https://youtube/w8j07_DBL_I',
+           'published_at' => Carbon::parse('December 13, 2020 8:00pm'),
+           'completed' => false,
+           'previous' => null,
+           'next' => null,
+           'series_id' => 1
+        ]);
+
+        // FASE 2 -> Execució -> Executa el codi a provar
+
+        $response = $this->get('/videos/' . $video->id);
+
+
+
+        // FASE 3 -> Assertions -> comprovacions
+        $response->assertStatus(200);
+        $response->assertSee('Ubuntu 101');
+        $response->assertSee('# Here description');
+        $response->assertSee('December 13');
+
+
+    }
+}
