252dbb4a73e9386fd177772089e3788d963d85f6
Palanka777 <dpont@iesebre.com>
2021-11-13 23:44:47 +0100

    video 107

--- tests/Unit/HelpersTest.php
+++ tests/Unit/HelpersTest.php
@@ -2,25 +2,59 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Hash;
+use Tests\TestCase;
 
 class HelpersTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * @test
      */
+
     public function create_default_user()
     {
-        $this->assertTrue(true);
+
+        create_default_user();
+
+        $this->assertDatabaseCount('users',1) ;
+        $this->assertDatabaseHas('users',[
+            'email'=> config('casteaching.default_user.email'),
+        ]) ;
+        $this->assertDatabaseHas('users',[
+            'name'=> config('casteaching.default_user.name'),
+        ]) ;
+
+
+        $user=User::find(1);
+        $this->assertNotNull($user);
+        $this->assertEquals(config('casteaching.default_user.email'), $user->email);
+        $this->assertEquals(config('casteaching.default_user.name'), $user->name);
+        $this->assertTrue(Hash::check(config('casteaching.default_user.password'), $user->password));
     }
+
+    /**
+     * @test
+     */
 
     public function create_default_profe_user()
     {
-        $this->assertTrue(true);
+
+
+        create_default_profe_user();
+        $this->assertDatabaseCount('users',1) ;
     }
 
+    /**
+     * @test
+     */
     public function create_default_video()
     {
-        $this->assertTrue(true);
+
+        create_default_video();
+        $this->assertDatabaseCount('videos',1) ;
     }
 }
