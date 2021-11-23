b794707cd49384a934cd75ae5c87f1a606bd65a4
Palanka777 <dpont@iesebre.com>
2021-11-23 16:59:55 +0100

    No va el css del tailwind

--- resources/views/videos/show.blade.php
+++ resources/views/videos/show.blade.php
@@ -1,7 +1,22 @@
 <x-casteaching-layout>
-    <p>{{ $video->title }}</p>
-    <ul>
-        <li>Description: {{ $video->description }}</li>
-        <li>Data: {{ $video->published_at->format('F d') }}</li>
-    </ul>
+    <div class="flex flex-col h-screen">
+
+        <iframe
+            class="md:p-6 h-4/5"
+            src="https://www.youtube.com/embed/btGr3mPK1dU?list=PLyasg1A0hpk07HA0VCApd4AGd3Xm45LQv"
+            title="YouTube video player" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen>
+
+        </iframe>
+
+        <div class="md:p-6">
+            <h2>{{ $video->title }}</h2>
+        </div>
+
+        <div class="md:p-6">
+            {{ $video->description }}
+        </div>
+    </div>
+
 </x-casteaching-layout>
