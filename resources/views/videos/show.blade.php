86c4bb12d51e3aa9f99f396a48252bf1475e9e76
Palanka777 <dpont@iesebre.com>
2021-11-23 17:13:09 +0100

    Fi del video 109 tot funcionant

--- resources/views/videos/show.blade.php
+++ resources/views/videos/show.blade.php
@@ -2,7 +2,7 @@
     <div class="flex flex-col h-screen">
 
         <iframe
-            class="md:p-6 h-4/5"
+            class="md:p-3 lg:p-5 xl:px10 xl:py5 2xl:px-20 2xl:py-10 h-4/5"
             src="https://www.youtube.com/embed/btGr3mPK1dU?list=PLyasg1A0hpk07HA0VCApd4AGd3Xm45LQv"
             title="YouTube video player" frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
@@ -10,11 +10,11 @@
 
         </iframe>
 
-        <div class="md:p-6">
-            <h2>{{ $video->title }}</h2>
+        <div class="p-4 lg:p-5 xl:p-10 2xl:p-20">
+            {{ $video->title }}
         </div>
 
-        <div class="md:p-6">
+        <div class="p-4 lg:p-5 xl:p-10 2xl:p-20">
             {{ $video->description }}
         </div>
     </div>
