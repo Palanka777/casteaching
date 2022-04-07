a96209b5de38a66935fa8b051fbea2d10cfa013b
Palanka777 <dpont@iesebre.com>
2022-04-07 15:51:09 +0200

    Acabo video 138 i afegeixo a la validacio el tamany maxim de 2 Megas

--- app/Http/Controllers/SeriesImageManageController.php
+++ app/Http/Controllers/SeriesImageManageController.php
@@ -14,6 +14,10 @@ class SeriesImageManageController extends Controller
 
     public function update(Request $request)
     {
+        $request->validate([
+            'image' => ['image','dimensions:min_height=400,ratio=16/9','max:2000'],
+        ]);
+
         $serie= Serie::findOrFail($request->id);
         $serie->image = $request->file('image')->store('series','public');
         $serie->save();
