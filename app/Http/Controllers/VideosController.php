3d7e6d3c8949edda16bd5f4cd409033101847040
Palanka777 <dpont@iesebre.com>
2022-05-05 16:57:17 +0200

    WIP

--- app/Http/Controllers/VideosController.php
+++ app/Http/Controllers/VideosController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Video;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Tests\Feature\Video\VideoTest;
 
 class VideosController extends Controller
